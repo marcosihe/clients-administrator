f2a2c077d8d13daa9bd10e0f5be7011badfb2115
unknown <hemrcs@gmail.com>
2022-06-27 11:35:43 -0300

    #003 pages
    - create the needed pages
    - create the form component

--- src/pages/NewClient.jsx
+++ src/pages/NewClient.jsx
@@ -1,7 +1,15 @@
+import Form from "../components/Form";
+
 const NewClient = () => {
   return (
-    <div>NewClient</div>
-  )
-}
+    <>
+      <div className="font-black text-4xl text-blue-900">Nuevo Cliente</div>
+      <p className="mt-3">
+        Completa los siguientes campos para registrar unn nuevo cliente
+      </p>
+      <Form />
+    </>
+  );
+};
 
-export default NewClient
+export default NewClient;
