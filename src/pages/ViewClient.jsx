8ccf28bf26991a8491b68ebe2ec7fa6ac199b6b6
unknown <hemrcs@gmail.com>
2022-06-28 00:55:07 -0300

    #007 view client
    - work on view client component
    - Add spinner to handle the asynchrony

--- src/pages/ViewClient.jsx
+++ src/pages/ViewClient.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CardText from "../components/CardText";
+import Spinner from "../components/Spinner";
 
 const ViewClient = () => {
   const { id } = useParams();
   const [client, setClient] = useState({});
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(!loading);
     const getClient = async () => {
       try {
         const URL = `http://localhost:4000/clients/${id}`;
@@ -16,20 +19,31 @@ const ViewClient = () => {
       } catch (error) {
         console.log(error);
       }
+      setTimeout( () => {
+        setLoading(false);
+      }, 1000)
     };
     getClient();
   }, []);
   return (
-    <div className="md:w-3/4 mx-auto py-5 px-2 bg-white text-white shadow-md rounded-md">
-      <h1 className="text-center font-black text-4xl text-blue-900 ">
-        Cliente
-      </h1>
-      <CardText param={"Nombre"} value={client.name} />
-      <CardText param={"Empresa"} value={client.company} />
-      <CardText param={"E-mail"} value={client.email} />
-      {client.phone && <CardText param={"Celular"} value={client.phone} />}
-      {client.notes && <CardText param={"Notas"} value={client.notes} />}
-    </div>
+    <>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="md:w-3/4 mx-auto py-5 px-2 bg-white text-white shadow-md rounded-md">
+          <h1 className="text-center font-black text-4xl text-blue-900 ">
+            {id == client.id ? 'Cliente' : 'Cliente no encontrado'}
+          </h1>
+          {client.name && <CardText param={"Nombre"} value={client.name} />}
+          {client.company && (
+            <CardText param={"Empresa"} value={client.company} />
+          )}
+          {client.email && <CardText param={"E-mail"} value={client.email} />}
+          {client.phone && <CardText param={"Celular"} value={client.phone} />}
+          {client.notes && <CardText param={"Notas"} value={client.notes} />}
+        </div>
+      )}
+    </>
   );
 };
 
